718bb6218d4fe89da536787471ca36896d310f74
türkay ürkmez <turkay.urkmez@outlook.com>
2025-09-22 15:50:16 +0300

    Add .NET 9.0 solution and filtering examples
    Introduce a new Visual Studio solution and project files for a .NET application targeting .NET 9.0. Add a `Helper` class with a `Filter` method to filter integers based on criteria. Update `Program.cs` to demonstrate filtering using a named method, an anonymous delegate, a lambda expression, and LINQ.

---
+++ DelegateAndLambda/DelegateAndLambda/Helper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateAndLambda
+{
+    public static class Helper
+    {
+      //  public delegate bool Criteria(int number);
+        public static int[] Filter(int[] numbers, Func<int,bool> criteria)
+        {
+            List<int> output = new List<int>();
+            foreach (var item in numbers)
+            {
+                if (criteria(item))
+                {
+                    output.Add(item);
+                }
+            }
+
+            return output.ToArray();
+        }
+    }
+}
---
+++ DelegateAndLambda/DelegateAndLambda/Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DelegateAndLambda;
+
+Console.WriteLine("Hello, World!");
+
+bool isEven(int number)
+{
+    return number % 2 == 0;
+}
+
+void showNumbers(int[] numbers)
+{
+
+    Console.WriteLine(string.Join(", ", numbers));
+}
+
+
+
+int[] numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+//.net 1.0:
+var evenNumbers = Helper.Filter(numbers, isEven);
+showNumbers(evenNumbers);
+
+//.net 2.0:
+var oddNumbers = Helper.Filter(numbers, delegate (int x) { return x % 2 == 1; });
+showNumbers(oddNumbers);
+
+//.net 3.5:
+var bigThanFive = Helper.Filter(numbers, s => s > 5);
+showNumbers(bigThanFive);
+
+var smallThanEight = numbers.Where(n => n < 8);
+
+showNumbers(smallThanEight.ToArray());
+
+
+//LINQ: Language INtegrated Query
+//SELECT num FROM numbers WHERE num < 8
+var smallThenEight_withLINQ = from num in numbers
+                              where num < 8
+                              select num;
+
+
+
+
