e0e6af1fb72f79d118ebe87e00cd118de7a55703
türkay ürkmez <turkay.urkmez@outlook.com>
2025-09-22 11:01:35 +0300

    Update project configuration and add sample console app
    - Updated `.gitattributes` for line endings and diff settings.
    - Expanded `.gitignore` to include various Visual Studio artifacts.
    - Created/modified `Types.sln` to define project configurations.
    - Added `Program.cs` with a simple console application demonstrating integer addition with overflow checking.
    - Updated `TypeOverview.csproj` to specify .NET project settings, including target framework and nullable reference types.

---
+++ Types/TypeOverview/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//byte x = 255;
+//byte y = 2;
+
+int z = getTotal(2147483647, 1);
+
+Console.WriteLine(z);
+
+int getTotal(int x, int y)
+{
+    int output = 0;
+	try
+	{
+		checked
+		{
+			output = x + y;
+		}
+	}
+	catch (OverflowException)
+	{
+
+        Console.WriteLine($"{x} ve {y} toplamı int tipine dönüştürülemedi!");
+	}
+
+    return output;
+
+
+    
+}
