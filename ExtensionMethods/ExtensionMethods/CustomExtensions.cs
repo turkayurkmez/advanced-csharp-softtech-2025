444b45991cc21535081c42bce53af47d62a254a3
türkay ürkmez <turkay.urkmez@outlook.com>
2025-09-22 16:03:50 +0300

    Add initial project structure and extension method
    Created the solution file `ExtensionMethods.sln` to define the project structure and configurations. Added `CustomExtensions.cs` with a `GetSquare` extension method for integers. Established the project file `ExtensionMethods.csproj` for .NET 9.0 and included a simple console application in `Program.cs` to demonstrate the extension method.

---
+++ ExtensionMethods/ExtensionMethods/CustomExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public static class CustomExtensions
+    {
+        public static int GetSquare(this int number) => number * number;
+    }
+}
---
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExtensionMethods;
+
+Console.WriteLine("Hello, World!");
+int x = 5;
+Console.WriteLine(x.GetSquare());
+
+Console.WriteLine(7.GetSquare());
+
