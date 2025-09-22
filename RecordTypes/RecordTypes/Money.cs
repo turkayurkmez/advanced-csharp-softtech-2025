60f5bd0195d19cbb4a68712809832fc5ba657c56
türkay ürkmez <turkay.urkmez@outlook.com>
2025-09-22 14:52:53 +0300

    Add RecordTypes project with Money and User records
    Created a new Visual Studio solution and project files.
    Added `Money` and `User` record types for better data handling.
    Updated `Program.cs` to demonstrate usage of the `Money` record.

---
+++ RecordTypes/RecordTypes/Money.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordTypes
+{
+    //public record Money
+    //{
+    //    //immutable bir nesne: Nesne oluşturulduktan sonra bir daha verileri set edemezsiniz.
+    //    public Money(decimal amount, string currency)
+    //    {
+    //        Amount = amount;
+    //        Currency = currency;
+    //    }
+    //    public decimal Amount { get; init; }
+    //    public string Currency { get; init; }
+    //}
+
+    public record Money(decimal Amount, string Currency);
+}
---
+++ RecordTypes/RecordTypes/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RecordTypes;
+
+Console.WriteLine("Hello, World!");
+
+User user = new User() { Id =1, Name ="Türkay"};
+User user1 = new User() { Id = 1, Name = "Türkay" };
+
+Console.WriteLine($"Eşit mi? {user == user1}");
+
+user1.Name = "Ümmühan";
+
+Console.WriteLine($"user1 instance'ının adı: {user1.Name} olarak değişti");
+Console.WriteLine($"Eşit mi? {user == user1}");
+
+
+Money money = new Money(100, "TL");
+Money tl = new Money(100, "TL");
+Money anotherMoney = new Money(1500, "Dollar");
+
+if (money == tl)
+{
+    Console.WriteLine("İki nesne de aynı değerde");
+}
+else
+{
+    Console.WriteLine("Farklı nesneler");
+}
+
