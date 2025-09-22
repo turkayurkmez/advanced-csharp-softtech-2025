9c1633bf8ca8ae39234d6c9ddb7b73bb42874919
türkay ürkmez <turkay.urkmez@outlook.com>
2025-09-22 12:05:10 +0300

    Add GenericTypes project with Point classes
    This commit introduces a new project named "GenericTypes" to the solution. It includes a new C# file, `Point.cs`, which defines the classes `Point`, `DoublePoint`, `ObjectPoint`, and `GenericPoint<T>`, each with properties for coordinates and a reset method. The `ObjectPoint` class features type-based reset logic. A project file, `GenericTypes.csproj`, is created with .NET SDK and targets .NET 9.0. The `Program.cs` file is updated to utilize the new classes.

---
+++ Types/GenericTypes/Point.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTypes
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public void Reset()
+        {
+            X = 0;
+            Y = 0;
+        }
+
+
+    }
+
+    public class DoublePoint 
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public void Reset() { 
+            X = 0.0;
+            Y = 0.0;
+        }
+    }
+
+    public class ObjectPoint
+    {
+        public object X { get; set; }
+        public object Y { get; set; }
+
+        public void Reset()
+        {
+            if (X is int && Y is int)
+            {
+                X = 0; 
+                Y = 0;
+            }
+            else if (X is double && Y is double)
+            {
+
+            }
+
+
+        }
+    }
+
+    public class GenericPoint<T> where T: struct, IComparable<T>
+    {
+        public T X { get; set; }
+        public T Y { get; set; }
+
+        public void Reset()
+        {
+            X = default(T);
+            Y = default(T);
+        }
+    }
+}
+
---
+++ Types/GenericTypes/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using GenericTypes;
+using System.Collections;
+
+Console.WriteLine("Hello, World!");
+
+object number = 42;
+
+
+int value = (int)number; 
+
+ArrayList items = new ArrayList();
+items.Add(true);
+items.Add(75);
+items.Add("Remzi");
+items.Add(1.65M);
+
+bool isApproved = (bool)items[3];
+
+
+object PushValue(object value)
+{
+    //bir şeyler...
+    return value;
+}
+
+
+PushValue(42);
+PushValue("Naber");
+PushValue(new Random());
+
+
+DoublePoint point = new DoublePoint();
+GenericPoint<decimal> decimalPoint = new GenericPoint<decimal>();
+GenericPoint<float> floatPoint = new GenericPoint<float>();
+
+GenericPoint<bool> stringPoint = new GenericPoint<bool>();
