df972882728d7acde77af903c1e2818fa5140f70
türkay ürkmez <turkay.urkmez@outlook.com>
2025-09-23 10:05:49 +0300

    Add Random extension methods for letter and password
    Updated `CustomExtensions.cs` to include `NextLetter` and `NextPassword` methods for generating random letters and passwords. Modified `Program.cs` to demonstrate the usage of these new methods alongside existing functionality.

--- ExtensionMethods/ExtensionMethods/CustomExtensions.cs
+++ ExtensionMethods/ExtensionMethods/CustomExtensions.cs
@@ -9,5 +9,30 @@ namespace ExtensionMethods
     public static class CustomExtensions
     {
         public static int GetSquare(this int number) => number * number;
+
+        public static string NextLetter(this Random random, bool isLower=false)
+        {
+            //65-90 97-122
+
+            int charValue = isLower ? random.Next(97, 123) : random.Next(65, 91);        
+            string letter = ((char)charValue).ToString();
+            return letter;
+
+          
+        }
+
+        public static string NextPassword(this Random random, int length = 6) {
+
+            string password = string.Empty;
+            for (int i = 0; i < length; i++)
+            {
+                var isLower = Convert.ToBoolean(random.Next(0, 2));
+                password += random.NextLetter(isLower);
+            }
+
+            return password;
+          
+
+        }
     }
 }
--- ExtensionMethods/ExtensionMethods/Program.cs
+++ ExtensionMethods/ExtensionMethods/Program.cs
@@ -6,4 +6,6 @@ int x = 5;
 Console.WriteLine(x.GetSquare());
 
 Console.WriteLine(7.GetSquare());
+Console.WriteLine(new Random().NextLetter());
+Console.WriteLine(new Random().NextPassword(8));
 
