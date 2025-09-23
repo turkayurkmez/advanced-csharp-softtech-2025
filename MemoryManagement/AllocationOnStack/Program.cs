9e59f523453e94af168ce9e45c11381bc795bad2
türkay ürkmez <turkay.urkmez@outlook.com>
2025-09-23 14:35:41 +0300

    Add AllocationOnStack project with memory allocation demo
    This commit introduces a new project called "AllocationOnStack" to the Visual Studio solution, targeting .NET 9.0 with implicit usings and nullable reference types enabled.
    
    Additionally, a console application in `Program.cs` demonstrates heap and stack memory allocations using an integer array and `Span<T>` with `stackalloc`. The code includes examples of slicing and indexing, showcasing modern C# features.

---
+++ MemoryManagement/AllocationOnStack/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//heap: 1M sayı heap'de
+int[] numbers = new int[1000];
+//stack allocation: 1M sayı stack'de
+Span<int> stackNumbers = stackalloc int[1000];
+for (int i = 0; i < stackNumbers.Length; i++)
+{
+    stackNumbers[i] = i * 2;
+}
+
+Console.WriteLine($"İlk eleman: {stackNumbers[0]}");
+Console.WriteLine($"Sondan ikinci eleman: {stackNumbers[^2]}");
+Console.WriteLine($"100.'den 120. elemana kadar: {string.Join(", ", stackNumbers[100..120].ToArray())}");
+Console.WriteLine($"Slicing sondan 120 ile sondan 100: {string.Join(", ", stackNumbers[^120..^100].ToArray())}");
