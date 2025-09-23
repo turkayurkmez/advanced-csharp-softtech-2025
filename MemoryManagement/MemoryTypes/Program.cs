75f92fa51861cd7efb0778e119ce1682cb01c7d8
türkay ürkmez <turkay.urkmez@outlook.com>
2025-09-23 14:19:53 +0300

    Add MemoryManagement solution and performance demo
    Introduce a new Visual Studio solution (`MemoryManagement.sln`) and project (`MemoryTypes.csproj`) targeting .NET 9.0. Update `Program.cs` to demonstrate string manipulation performance using traditional methods versus `ReadOnlySpan<char>` and `Memory<char>`, generating a million email addresses for comparison.

---
+++ MemoryManagement/MemoryTypes/Program.cs
@@ -0,0 +1,61 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+Console.WriteLine("Hello, World!");
+string email = "turkay.urkmez@dinamikzihin.com";
+
+
+
+//bellek maaliyeti yüksek ve GC açısından dezavantaj:
+string userName = email.Substring(0, email.IndexOf('@'));
+string domain = email.Substring(email.IndexOf('@')+1);
+Console.WriteLine($"Kullanıcı adı: {userName} ve domain: {domain}");
+
+
+
+
+ReadOnlySpan<char> emailspan = email.AsSpan();
+int atIndex = emailspan.IndexOf("@");
+ReadOnlySpan<char> userNameSpan = emailspan.Slice(0,atIndex);
+Console.WriteLine(userNameSpan.ToString());
+
+
+
+
+string[] generateMillionEmails()
+{
+    string[] emails = new string[1000000];
+
+    for (int i = 0; i<emails.Length;i++)
+    {
+        emails[i] = $"sample_{i}@domain{i}.com";
+    }
+
+    return emails;
+}
+
+string[] emails = generateMillionEmails();
+var stopWatch = Stopwatch.StartNew();
+for (int i = 0; i < emails.Length; i++)
+{
+    string subUserName = emails[i].Substring(0, emails[i].IndexOf('@'));
+
+}
+
+stopWatch.Stop();
+Console.WriteLine($"Substring ile geçen süre: {stopWatch.ElapsedMilliseconds} ms");
+
+stopWatch.Restart();
+for (int i = 0; i < emails.Length; i++)
+{
+    ReadOnlySpan<char> emailWithSpan = emails[i].AsSpan();
+    ReadOnlySpan<char> spanUserName = emailWithSpan.Slice(0, emailWithSpan.IndexOf('@'));
+
+}
+
+stopWatch.Stop();
+Console.WriteLine($"Span<> ile geçen süre: {stopWatch.ElapsedMilliseconds} ms");
+
+
+//Memory -> Span struct'unun asenkron kardeşi. Eğer bir task içinde bu işlemi yapıyorsanız; span yerine kullanılır.
+var emailMemory = email.AsMemory();
