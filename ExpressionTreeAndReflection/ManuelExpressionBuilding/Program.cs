b557ccb2109f870d419957fdfa3d4672cb1ccbc9
türkay ürkmez <turkay.urkmez@outlook.com>
2025-09-24 12:20:59 +0300

    Add ManuelExpressionBuilding project and example code
    This commit introduces a new project called "ManuelExpressionBuilding" to the solution `ExpressionTreeAndReflection.sln`, targeting .NET 9.0. The project includes a `.csproj` file with necessary configurations for a console application.
    
    Additionally, a new `Program.cs` file has been created, demonstrating the use of expression trees in C#. The code performs a mathematical operation and outputs the result, with comments provided in Turkish for clarity.

---
+++ ExpressionTreeAndReflection/ManuelExpressionBuilding/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Linq.Expressions;
+
+Console.WriteLine("Hello, World!");
+// (x + y) * 2
+//1. Adım: Parametreler
+ParameterExpression x =  Expression.Parameter(typeof(int), "x");
+ParameterExpression y = Expression.Parameter(typeof(int), "y");
+
+//2. Adım: Sabit değer
+ConstantExpression two = Expression.Constant(2);
+//3. Adım: Toplama
+BinaryExpression addition = Expression.Add(x, y);
+//4. Adım: Çarpma
+BinaryExpression multiply = Expression.Multiply(addition, two);
+
+//5. Adım Lamda:
+Expression<Func<int, int, int>> manualLambda = Expression.Lambda<Func<int, int, int>>(multiply, x, y);
+Console.WriteLine($"Lambda temsili: {manualLambda}");
+var compiled = manualLambda.Compile();
+int result = compiled(5, 3);
+
+Console.WriteLine($"sonuç: {result}");
+
+
