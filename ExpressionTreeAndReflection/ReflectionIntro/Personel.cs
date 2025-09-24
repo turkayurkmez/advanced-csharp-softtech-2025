ede1152666e3003885ab076b310de5c8809c02e2
türkay ürkmez <turkay.urkmez@outlook.com>
2025-09-24 12:37:12 +0300

    Add ReflectionIntro project with Personel class
    - Introduced a new project "ReflectionIntro" in the solution.
    - Created the `Personel` class with properties, a private field, and methods.
    - Updated `Program.cs` to demonstrate reflection on the `Personel` class.
    - Added `ReflectionIntro.csproj` targeting .NET 9.0 with necessary configurations.

---
+++ ExpressionTreeAndReflection/ReflectionIntro/Personel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReflectionIntro
+{
+    public class Personel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        private decimal salary;
+
+        public void Work()
+        {
+            Console.WriteLine($"{Name} isimli çalışan iş başında");
+        }
+
+        private void secretMethod()
+        {
+            Console.WriteLine("Bu private bir metot!");
+        }
+    }
+}
---
+++ ExpressionTreeAndReflection/ReflectionIntro/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ReflectionIntro;
+using System.Reflection;
+
+Console.WriteLine("Hello, World!");
+
+Personel personel = new Personel { Age = 35, Name = "Hülya" };
+
+Type personType = personel.GetType();
+
+Console.WriteLine("--- Properties ---");
+foreach (var property in personType.GetProperties())
+{
+    Console.WriteLine($"{property.Name}: {property.GetValue(personel)} ({property.PropertyType.Name})");
+
+}
+
+Console.WriteLine("--- Methods ---");
+foreach (var method in personType.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance))
+{
+    if (method.DeclaringType == personType)
+    {
+        Console.WriteLine($"{method.Name} - {(method.IsPublic ? "public": "private")}");
+
+    }
+}
+
+MethodInfo methodInfo = personType.GetMethod("secretMethod", BindingFlags.NonPublic | BindingFlags.Instance);
+methodInfo?.Invoke(personel, null);
